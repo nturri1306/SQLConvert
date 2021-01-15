f34bf6b3131c9de1063870be23085f14199a5d29
nturri1306 <nturri1306@gmail.com>
2021-01-15 18:46:14 +0100

    Aggiungere i file di progetto.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIn = new System.Windows.Forms.TextBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIn
+            // 
+            this.textBoxIn.Location = new System.Drawing.Point(12, 72);
+            this.textBoxIn.Multiline = true;
+            this.textBoxIn.Name = "textBoxIn";
+            this.textBoxIn.Size = new System.Drawing.Size(760, 240);
+            this.textBoxIn.TabIndex = 0;
+            this.textBoxIn.Leave += new System.EventHandler(this.textBoxIn_Leave);
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(12, 372);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(760, 377);
+            this.textBoxOut.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TEXTIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SQL RESULT";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(15, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "ANDROID";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(652, 318);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(120, 48);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "CLEAR";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.textBoxIn);
+            this.Name = "Form1";
+            this.Text = "SQLConvert 0.0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIn;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonClear;
+    }
+
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+            private  string ReplaceFirst( string str, string oldValue, string newValue)
+            {
+                int startindex = str.IndexOf(oldValue);
+
+                if (startindex == -1)
+                {
+                    return str;
+                }
+
+                return str.Remove(startindex, oldValue.Length).Insert(startindex, newValue);
+            }
+        
+
+        public List<int> GetPositions(string source, string searchString)
+        {
+            List<int> ret = new List<int>();
+            int len = searchString.Length;
+            int start = -len;
+            while (true)
+            {
+                start = source.IndexOf(searchString, start + len);
+                if (start == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    ret.Add(start);
+                }
+            }
+            return ret;
+        }
+
+    
+
+
+        private void textBoxIn_Leave(object sender, EventArgs e)
+        {
+            string[] stringSeparators = new string[] { "\r\n" };
+
+           // ==> :
+
+            ArrayList param = new ArrayList();
+            ArrayList paramName = new ArrayList();
+            String strMerge = "";
+
+            string[] lines = textBoxIn.Text.Trim().Split(stringSeparators, StringSplitOptions.None);
+
+
+
+            Boolean isParamName = textBoxIn.Text.ToUpper().IndexOf("INSERT") >= 0;
+
+
+            foreach (string s in lines)
+            {
+                string str = s.Substring(s.IndexOf("):") + 2);
+
+                if (str.IndexOf("==> param") > 0  || str.IndexOf("==>") > 0)
+                {
+
+
+
+
+                    if (isParamName)
+                    {
+                        int inizio = s.IndexOf("==> :")+5;
+                        int fine = s.IndexOf("= ",inizio);
+
+                        string name = s.Substring(inizio, fine - inizio);
+                        paramName.Add(name.Trim());
+
+                        fine = fine + 2;
+
+                        try
+                        {
+
+                            int fine2 = s.IndexOf("'", fine + 1);
+
+                            string value = s.Substring(fine + 1, fine2 - fine - 1);
+
+                            param.Add(value);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            param.Add("null");
+                        
+                        }
+
+                        }
+                    else
+                    {
+                        string value = str.Substring(str.IndexOf(":") + 1);
+
+                        param.Add(value);
+                    }
+
+                }
+                else
+                {
+                    if (str.ToUpper().Contains("INSERT") 
+                        || str.ToUpper().Contains("SELECT")
+                        || str.ToUpper().Contains("UPDATE")
+                        || str.ToUpper().Contains("DELETE"))
+
+                        strMerge += str + "\r\n";
+                }
+
+
+
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine(strMerge);
+
+            int i = 0;
+
+            foreach (String value in param)
+            {
+
+                if (!isParamName)
+
+                    strMerge = ReplaceFirst(strMerge, "?", value);
+                else
+
+                {
+
+                    string key = ":" + paramName[i].ToString().Trim();
+
+                    strMerge = strMerge.Replace(key+",","'"+value+"',");
+                    strMerge = strMerge.Replace(key + ")", "'" + value + "')");
+
+                }
+                i++;
+
+            }
+
+            //List<int> list = GetPositions(strMerge, "?");
+
+            textBoxOut.Text= strMerge;
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxIn.Text = "";
+            textBoxOut.Text = ""; 
+        }
+    }
+
